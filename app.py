7e79a8e7a364a6f05679a0b688e6eb0c9638761b
Cryptic Loop <nickstride1@gmail.com>
2019-01-07 15:17:44 +0000

    Data populating landing page

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+sites = ["facebook.com","amazon.co.uk","youtube.com","netflix.com","google.com"]
+
+
 @app.route("/")
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', sites=sites)
 
 if __name__ == "__main__":
     app.run(debug=True)
