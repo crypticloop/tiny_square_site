8d34f68e34493838d5ec722f5a82220b5368e32a
Cryptic Loop <nickstride1@gmail.com>
2019-01-07 23:13:19 +0000

    Added a float left page

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-sites = ["facebook.com","amazon.co.uk","youtube.com","netflix.com","google.com","w3schools.com", "github.com","rolls-royce.com","getbootstrap.com","webflow.com","asos.com","taobao.com","qq.com","reddit.com","yahoo.com","wikipedia.org","baidu.com"]
+sites = ["facebook.com","amazon.co.uk","youtube.com","netflix.com","google.com","w3schools.com", "github.com","getbootstrap.com","webflow.com","asos.com","taobao.com","qq.com","reddit.com","yahoo.com","wikipedia.org","baidu.com"]
 
 
 @app.route("/")
 def hello():
     return render_template('home.html', sites=sites)
+
+@app.route("/float")
+def floatleft():
+    return render_template('floatleft.html', sites=sites)
 
 if __name__ == "__main__":
     app.run(debug=True)
