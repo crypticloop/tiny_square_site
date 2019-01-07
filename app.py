9cd5f0fbd4e10b48c07532a1998196497398063b
Cryptic Loop <nickstride1@gmail.com>
2019-01-07 17:44:08 +0000

    Added more websites

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-sites = ["facebook.com","amazon.co.uk","youtube.com","netflix.com","google.com","w3schools.com", "github.com","rolls-royce.com"]
+sites = ["facebook.com","amazon.co.uk","youtube.com","netflix.com","google.com","w3schools.com", "github.com","rolls-royce.com","getbootstrap.com","webflow.com","asos.com","taobao.com","qq.com","reddit.com","yahoo.com","wikipedia.org","baidu.com"]
 
 
 @app.route("/")
